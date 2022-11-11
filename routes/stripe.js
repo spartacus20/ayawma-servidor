e73ec556f9f7452f40e374b2c79f9d67756877e9
Spartacus20 <you@example.com>
2022-11-11 20:00:57 +0000

    some changes

--- routes/stripe.js
+++ routes/stripe.js
@@ -17,7 +17,7 @@ router.post('/create-checkout-session', async (req, res) => {
   const line_items = req.body.basket.map((item) => {
     return {
       price_data: {
-        currency: "usd",
+        currency: "EUR",
         product_data: {
           name: item.title,
          // images: [item.image],
@@ -36,7 +36,7 @@ router.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     shipping_address_collection: {
-      allowed_countries: ['US', 'CA'],
+      allowed_countries: ["AC", "AD", "AE", "AF", "AG", "AI", "AL", "AM", "AO", "AQ", "AR", "AT", "AU", "AW", "AX", "AZ", "BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BV", "BW", "BY", "BZ", "CA", "CD", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CV", "CW", "CY", "CZ", "DE", "DJ", "DK", "DM", "DO", "DZ", "EC", "EE", "EG", "EH", "ER", "ES", "ET", "FI", "FJ", "FK", "FO", "FR", "GA", "GB", "GD", "GE", "GF", "GG", "GH", "GI", "GL", "GM", "GN", "GP", "GQ", "GR", "GS", "GT", "GU", "GW", "GY", "HK", "HN", "HR", "HT", "HU", "ID", "IE", "IL", "IM", "IN", "IO", "IQ", "IS", "IT", "JE", "JM", "JO", "JP", "KE", "KG", "KH", "KI", "KM", "KN", "KR", "KW", "KY", "KZ", "LA", "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY", "MA", "MC", "MD", "ME", "MF", "MG", "MK", "ML", "MM", "MN", "MO", "MQ", "MR", "MS", "MT", "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NG", "NI", "NL", "NO", "NP", "NR", "NU", "NZ", "OM", "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM", "PN", "PR", "PS", "PT", "PY", "QA", "RE", "RO", "RS", "RU", "RW", "SA", "SB", "SC", "SE", "SG", "SH", "SI", "SJ", "SK", "SL", "SM", "SN", "SO", "SR", "SS", "ST", "SV", "SX", "SZ", "TA", "TC", "TD", "TF", "TG", "TH", "TJ", "TK", "TL", "TM", "TN", "TO", "TR", "TT", "TV", "TW", "TZ", "UA", "UG", "US", "UY", "UZ", "VA", "VC", "VE", "VG", "VN", "VU", "WF", "WS", "XK", "YE", "YT", "ZA", "ZM", "ZW",],
     },
     shipping_options: [
       {
@@ -44,7 +44,7 @@ router.post('/create-checkout-session', async (req, res) => {
           type: 'fixed_amount',
           fixed_amount: {
             amount: 0,
-            currency: 'usd',
+            currency: 'EUR',
           },
           display_name: 'Free shipping',
           // Delivers between 5-7 business days
@@ -65,7 +65,7 @@ router.post('/create-checkout-session', async (req, res) => {
           type: 'fixed_amount',
           fixed_amount: {
             amount: 1500,
-            currency: 'usd',
+            currency: 'EUR',
           },
           display_name: 'Next day air',
           // Delivers in exactly 1 business day
